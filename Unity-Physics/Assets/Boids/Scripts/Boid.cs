4a454eeae8a1b5f956024d821688277f58bfc43c
MaxGuidry <mguidry7@outlook.com>
2017-11-08 23:23:33 -0600

    commit before I crash it

--- Unity-Physics/Assets/Boids/Scripts/Boid.cs
+++ Unity-Physics/Assets/Boids/Scripts/Boid.cs
@@ -2,11 +2,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using System.Threading;
 using UnityEngine;
 
-namespace Max   
+namespace Max
 {
-    
+
     public class Boid : Agent
     {
         public List<Boid> neighbors = new List<Boid>();
@@ -15,33 +16,45 @@ namespace Max
             velocty += acceleration * Time.fixedDeltaTime;
             if (velocty.magnitude > maxSpeed)
                 velocty = velocty.normalized * maxSpeed;
-            position += velocty * Time.fixedDeltaTime; 
-            
+            position += velocty * Time.fixedDeltaTime;
+            acceleration = Vector3.zero;
             return position;
 
         }
 
         public void AddNeighbor(Boid neighbor)
         {
-            if(!neighbors.Contains(neighbor))
+            if (neighbor == null)
+                return;
+            if (!neighbors.Contains(neighbor))
                 neighbors.Add(neighbor);
         }
 
         public Vector3 Cohesion()
         {
-            Vector3 force;
-            if(neighbors.Count == 0)
+            Vector3 force = new Vector3();
+            //new Thread(() =>
+            //{
+            //    Thread.CurrentThread.IsBackground = true;
+            if (neighbors.Count == 0)
+            {
+                
                 return Vector3.zero;
+            }
             Vector3 com = new Vector3();
             foreach (var neighbor in neighbors)
             {
                 com += neighbor.position;
             }
+
             com = com / neighbors.Count;
-            force = this.position - com;
+            //Debug.DrawLine(this.position,com,Color.black);
+            force = com - this.position;
+
+            //  }).Start();
             return force;
         }
 
-       
+
     }
 }
--- Unity-Physics/Assets/Boids/Scripts/BoidBehavior.cs
+++ Unity-Physics/Assets/Boids/Scripts/BoidBehavior.cs
@@ -1,21 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading;
+using UnityEngine.Assertions.Must;
 
 namespace Max
 {
 
     public class BoidBehavior : AgentBehavior
     {
-
+        private Thread t;
         public void SetAgent(Agent agent)
         {
-           
+
             a = agent;
         }
         // Use this for initialization
         void Start()
         {
+           
+            if (a == null)
+
+            {
+                a = new Boid();
+                a.Initialize(1, 2, new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10)));
+            }
+
+            foreach (var boid in GameObject.FindObjectsOfType<BoidBehavior>())
+            {
+                if (this.gameObject == boid.gameObject)
+                    continue;
+                if ((boid.gameObject.transform.position - this.transform.position).magnitude < 5f)
+                    (a as Boid).AddNeighbor(boid.a as Boid);
+            }
+            t= new Thread(() =>
+            {
+                while (true)
+                {
+                    Thread.CurrentThread.IsBackground = true;
+                    Vector3 f = .1f * (a as Boid).Cohesion();
+                    a.Add_Force(f);
+                }
+            });
+            t.Start();
         }
 
         // Update is called once per frame
@@ -23,12 +50,12 @@ namespace Max
         {
             foreach (var boid in GameObject.FindObjectsOfType<BoidBehavior>())
             {
-                if(this.gameObject == boid.gameObject)
+                if (this.gameObject == boid.gameObject)
                     continue;
-                if((boid.gameObject.transform.position - this.transform.position).magnitude < 5f)
+                if ((boid.gameObject.transform.position - this.transform.position).magnitude < 15f)
                     (a as Boid).AddNeighbor(boid.a as Boid);
             }
-            a.Add_Force( .01f * (a as Boid).Cohesion());
+    
         }
         void FixedUpdate()
         {
